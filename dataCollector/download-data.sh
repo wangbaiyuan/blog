1adc77c99723440fa475eec20db9f0ab09dd7a80
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-16 16:03:55 +0800

    fix download script

--- dataCollector/download-data.sh
+++ dataCollector/download-data.sh
@@ -4,7 +4,7 @@ set -e
 download() {
   echo "Download '$1' from qiniu..."
   downloadUrl=$(node ./dataCollector/lib/downloadDataFromQiniu.js $1)
-  curl ${downloadUrl} -f -s -I -w "Response: %{http_code}\n" -o "source/_data/$1"
+  curl ${downloadUrl} -f -s -w "Response: %{http_code}\n" -o "source/_data/$1"
 }
 download wp-comments.json
 download github-comments.json
