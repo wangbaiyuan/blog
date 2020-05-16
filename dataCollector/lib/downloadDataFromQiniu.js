4a54b8cf15d8875db368709fe3f12cc0a72199e4
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-16 21:24:32 +0800

    add random posts and recent comments

--- dataCollector/lib/downloadDataFromQiniu.js
+++ dataCollector/lib/downloadDataFromQiniu.js
@@ -8,6 +8,6 @@ const privateBucketDomain = process.env.QINIU_BUCKET_DOMAIN;
 const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 const config = new qiniu.conf.Config();
 const bucketManager = new qiniu.rs.BucketManager(mac, config);
-const deadline = parseInt(Date.now() / 1000) + 60; // Expired after 1 min
+const deadline = parseInt(Date.now() / 1000) + 300; // Expired after 1 min
 const privateDownloadUrl = bucketManager.privateDownloadUrl(privateBucketDomain, args[0], deadline);
 console.log(privateDownloadUrl);
