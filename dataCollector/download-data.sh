e6ce34c5a31b51624b7242f54aab9cd6ed822fef
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-16 15:57:55 +0800

    download dynamic data from qiniu

--- dataCollector/download-data.sh
+++ dataCollector/download-data.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-echo "Download data from qiniu..."
+set -e
+
+download() {
+  echo "Download '$1' from qiniu..."
+  downloadUrl=$(node ./dataCollector/lib/downloadDataFromQiniu.js $1)
+  curl ${downloadUrl} -f -s -I -w "Response: %{http_code}\n" -o "source/_data/$1"
+}
+download wp-comments.json
+download github-comments.json
---
+++ dataCollector/lib/downloadDataFromQiniu.js
@@ -0,0 +1,13 @@
+const qiniu = require('qiniu');
+
+var args = process.argv.slice(2);
+
+const accessKey = process.env.QINIU_KEYID;
+const secretKey = process.env.QINIU_KEYSECRET;
+const privateBucketDomain = process.env.QINIU_BUCKET_DOMAIN;
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+const config = new qiniu.conf.Config();
+const bucketManager = new qiniu.rs.BucketManager(mac, config);
+const deadline = parseInt(Date.now() / 1000) + 60; // Expired after 1 min
+const privateDownloadUrl = bucketManager.privateDownloadUrl(privateBucketDomain, args[0], deadline);
+console.log(privateDownloadUrl);
