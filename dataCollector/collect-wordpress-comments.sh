ee60f32465f1fb6a54d8d95523a0290c552465df
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-17 19:25:31 +0800

    output response code when collect wordpress comments

--- dataCollector/collect-wordpress-comments.sh
+++ dataCollector/collect-wordpress-comments.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 echo "Collecting comments from Wordpress..."
-curl -u "github-action:${WP_TOKEN}" ${WP_COMMENTS_API_URL} -o "source/_data/wp-comments.json"
+curl -u "github-action:${WP_TOKEN}" ${WP_COMMENTS_API_URL}\
+ -f -s -w "Response: %{http_code}\n" -o "source/_data/wp-comments.json"
