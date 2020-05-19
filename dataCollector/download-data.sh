a0cf911fb575c71d2fd680e418c00a703f8d5fa1
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-19 08:26:55 +0800

    (feat) add downloadDataFromQiniu.js back

--- dataCollector/download-data.sh
+++ dataCollector/download-data.sh
@@ -5,6 +5,7 @@ download() {
   echo "Download '$1' from qiniu..."
   downloadUrl=$(node ./dataCollector/lib/downloadDataFromQiniu.js $1)
   curl ${downloadUrl} -f -s -w "Response: %{http_code}\n" -o "source/_data/$1"
+  echo "Saved '$1' to 'source/_data/$1'"
 }
 download wp-comments.json
 download github-comments.json
