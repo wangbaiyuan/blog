977f6baa8884196979e4d647e6b92261e0e92cab
Baiyuan Wang <wangbaiyuan@bytedance.com>
2022-12-02 14:05:07 +0800

    chore: fix download data

--- dataCollector/lib/downloadDataFromQiniu.js
+++ dataCollector/lib/downloadDataFromQiniu.js
@@ -8,6 +8,7 @@ const privateBucketDomain = process.env.QINIU_BUCKET_DOMAIN;
 const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 const config = new qiniu.conf.Config();
 const bucketManager = new qiniu.rs.BucketManager(mac, config);
+
 const deadline = parseInt(Date.now() / 1000) + 300; // Expired after 1 min
 const privateDownloadUrl = bucketManager.privateDownloadUrl(privateBucketDomain, args[0], deadline);
 console.log(privateDownloadUrl);
