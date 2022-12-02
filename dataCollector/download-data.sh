87f1b55d3534f15625803eb1328e7b6aeae33521
Baiyuan Wang <wangbaiyuan@bytedance.com>
2022-12-02 13:45:42 +0800

    chore: fix download data

--- dataCollector/download-data.sh
+++ dataCollector/download-data.sh
@@ -4,6 +4,7 @@ set -e
 download() {
   echo "Download '$1' from qiniu..."
   downloadUrl=$(node ./dataCollector/lib/downloadDataFromQiniu.js $1)
+  echo "downloadUrl: ${downloadUrl} ";
   curl ${downloadUrl} -f -s -w "Response: %{http_code}\n" -o "source/_data/$1"
   echo "Saved '$1' to 'source/_data/$1'"
 }
